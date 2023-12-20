4695510fc7ddba0bd7c459d5fdb1ee0764865546
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-12-20 12:53:30 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+
+#include <clocale>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "");
+
+	int n, k, l;
+
+	cout << "Введите значение n: ";
+
+	cin >> n;
+
+	cout << "Введите значение k: ";
+
+	cin >> k;
+
+	cout << "Введите значение l: ";
+
+	cin >> l;
+
+	int* arr = new int[n];
+
+	cout << "Заполните массив: ";
+
+	for (int i = 0; i < n; i++) {
+
+		cin >> arr[i];
+
+	}
+
+	int sum = 0;
+
+	for (int i = k; i <= l; i++) {
+
+		sum += arr[i];
+
+	}
+
+	cout << "Сумма элементов массива с номерами от K до L включительно = " << sum << endl;
+
+	return 0;
+
+}
